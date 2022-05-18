6ea3be435010c4664ac2a7bc800e444f7fe365e2
Edmundo Fuentes <me@edmundofuentes.com>
2022-05-17 19:31:05 -0500

    updating dependencies

--- publish.sh
+++ publish.sh
@@ -4,12 +4,13 @@ echo "## Sculpin Generate"
 vendor/bin/sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
+# TODO: Sculpin is broken and prints out wrong "page" URLs
 ## Rename .html.twig files to .html only
-echo "FIX: Cleaning up the filenames"
-cd output_prod
-find . -name '*.html.twig' -exec sh -c 'mv "$0" "${0%.html.twig}.html"' {} \;
-cd ..
+#echo "FIX: Cleaning up the filenames"
+#cd output_prod
+#find . -name '*.html.twig' -exec sh -c 'mv "$0" "${0%.html.twig}.html"' {} \;
+#cd ..
 
 echo "## Netlify Deploy"
-# push it to netlify using the newer netlifyctl tool
-netlifyctl deploy -P output_prod
+# push it to netlify using the newer netlify-cli tool
+netlify deploy --prod --dir output_prod --site edmundofuentes
